0d0bece3cefc8524f48ac16f0976914143e9a101
manel-A-tech <manelameziane215@gmail.com>
2026-04-16 23:55:18 +0100

    'done'

--- src/components/alert details/AlertDetails.tsx
+++ src/components/alert details/AlertDetails.tsx
@@ -11,7 +11,7 @@ type AlertDetailsprops ={
 
 function AlertDetails({ camera , zone} : AlertDetailsprops){
   return(
-    <div className="pt-5 pl-4 pr-4 gap-4 grid grid-cols-12 bg-[#F4F6F9]">
+    <div className=" grid grid-cols-12 bg-[#F4F6F9]">
       <LiveFeed  zone={zone} camera={camera}/>
       <StatusVerification/>
       <AiStream zone={zone}/>
